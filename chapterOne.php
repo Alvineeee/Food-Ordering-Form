ab8dd831be7ca87c32fd56d0fde5d25e5798f79d
Alvineeee <115349458+Alvineeee@users.noreply.github.com>
2024-10-13 11:39:18 +0300

    Add files via upload

---
+++ chapterOne.php
@@ -0,0 +1,11 @@
+<?php
+// Chapter one practical Task
+$reg_no = "CIT-223-008/2022";
+$course_name = "Computer Science";
+$technology_name = "Web Development";
+
+echo "<h1> Roll Number: $reg_no </p>";
+echo "<h1> Course : $course_name </p>";
+echo "<h1> Technology: $technology_naame </p>";
+
+ ?>
