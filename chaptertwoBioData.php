da0fd0490de57457ee5c763c411ad42a43c7846f
Alvineeee <115349458+Alvineeee@users.noreply.github.com>
2024-10-13 12:31:37 +0300

    Add files via upload

---
+++ chaptertwoBioData.php
@@ -0,0 +1,22 @@
+<?php
+// Biodata Variables
+$name = "John Doe";
+$dob = "January 1, 1990";
+$gender = "Male";
+$marital_status = "Single";
+$nationality = "Kenyan";
+$contact_number = "0723456781";
+$email = "johndoe@gmail.com";
+
+
+// Display Biodata
+echo "<h2>Biodata</h2>";
+echo "<p><strong>Name:</strong> $name</p>";
+echo "<p><strong>Date of Birth:</strong> $dob</p>";
+echo "<p><strong>Gender:</strong> $gender</p>";
+echo "<p><strong>Marital Status:</strong> $marital_status</p>";
+echo "<p><strong>Nationality:</strong> $nationality</p>";
+echo "<p><strong>Contact Number:</strong> $contact_number</p>";
+echo "<p><strong>Email Address:</strong> $email</p>";
+
+
