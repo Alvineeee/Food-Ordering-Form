976d6aff276a98301db7279c5e9380bca8bf644f
Alvineeee <115349458+Alvineeee@users.noreply.github.com>
2024-10-13 15:59:43 +0300

    Add files via upload

---
+++ calculator.php
@@ -0,0 +1,36 @@
+<?php
+function calculator(){
+    echo "Select Operattion /n";
+    echo "1. Add/n";
+    echo "2. Subtract/n";
+    echo "3. Multiply/n";
+    echo "4. Divide/n";
+
+    $choice = readline("Enter choice(1/2/3/4): ");
+
+    // prompt input from use
+    $num1 = readline("Enter  first number: ");
+    $num2 = readline("Enter  second number: ");
+
+    if($choice == '1') {
+        echo "$num1 + $num2 is ". ($num1 + $num2) . "/n";
+    }elseif($choice == '2') {
+        echo "$num1 - $num2 is ". ($num1 - $num2) . "/n";
+    }elseif($choice == '3') {
+        echo "$num1 * $num2 is ". ($num1 * $num2) . "/n";
+    }elseif($choice == '4'){
+        if($num2 != 0)
+        {
+            echo "$num1 / $num2 is ". ($num1 / $num2) . "/n";
+        }else{
+            echo "ERROR!: Division by zero \n";
+        }
+        
+    }else{
+        echo "Invalid Choice";
+    }
+
+}
+
+calculator()
+?>
